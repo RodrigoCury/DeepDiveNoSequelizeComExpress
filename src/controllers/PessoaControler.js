c93579d170a8c7d5a10fd19d1ccdaf7b197945ef
Rodrigo Cury <rodrigocury@ufu.br>
2021-07-28 12:12:29 -0300

    Added CRUD Methods

--- src/controllers/PessoaControler.js
+++ src/controllers/PessoaControler.js
@@ -1,8 +1,45 @@
 const database = require('../models')
 
+// DAO
 class PessoaControler {
     static async getAll() {
         return await database.Pessoas.findAll()
+    }
+
+    static async getOne(id) {
+        return await database.Pessoas.findOne({
+            where: { id },
+            raw: true
+        })
+    }
+
+    static async createOne(pessoa) {
+        await database.Pessoas.create({
+            nome: pessoa.nome,
+            ativo: pessoa.ativo,
+            email: pessoa.email,
+            role: pessoa.role,
+            createdAt: Date.now(),
+            updatedAt: Date.now(),
+        })
+    }
+
+    static async updateOne(id, pessoa) {
+        return await database.Pessoas.update(
+            pessoa
+            , {
+                where: {
+                    id: id
+                }
+            })
+    }
+
+    static async deleteOne(id) {
+        await database.Pessoas.destroy({
+            where: {
+                id: id
+            }
+        })
     }
 }
 
--- src/routes/pessoasRouter.js
+++ src/routes/pessoasRouter.js
@@ -16,4 +16,55 @@ router.get('/', async (req, res, next) => {
     }
 })
 
+// GET by ID
+router.get('/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id
+        const pessoa = await PessoaControler.getOne(id)
+        res.status(200).json(pessoa)
+    } catch (error) {
+        res.status(400).json({
+            mensagem: error.message
+        })
+    }
+})
+
+// POST new Pessoa
+router.post('/', async (req, res, next) => {
+    try {
+        const pessoa = req.body
+        await PessoaControler.createOne(pessoa)
+        res.status(204).end()
+    } catch (error) {
+        res.status(400).json({
+            mensagem: error.message
+        })
+    }
+})
+
+// PUT on Pessoas by ID
+router.put('/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id
+        const pessoa = req.body
+        await PessoaControler.updateOne(id, pessoa)
+        res.status(204).end()
+    } catch (error) {
+        res.status(400).json({
+            mensagem: error.message
+        })
+    }
+})
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id
+        await PessoaControler.deleteOne(id)
+        res.status(204).end()
+    } catch (error) {
+        res.status(400).json({
+            mensagem: error.message
+        })
+    }
+})
 module.exports = router
